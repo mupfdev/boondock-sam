46964b330b5c5244607cb2b138435f080354f6ab
Josh Junon <junon@zeit.co>
2018-06-30 21:03:38 -0600

    Add proper platform detection and MacOS building instructions

--- config.mk
+++ config.mk
@@ -1,20 +1,26 @@
-TOOLCHAIN=
-#TOOLCHAIN=i686-w64-mingw32
-CC=$(TOOLCHAIN)-cc
 PROJECT=boondock-sam
-OUT=$(PROJECT)
-#OUT=$(PROJECT).exe
+
+ifeq ($(OS),Windows_NT)
+	OUT=$(PROJECT).exe
+	TOOLCHAIN=i686-w64-mingw32
+	CC=$(TOOLCHAIN)-cc
+else
+	OUT=$(PROJECT)
+	TOOLCHAIN=local
+	UNAME_S := $(shell uname -s)
+endif
+
 LIBS=\
-	-lSDL2 -lpthread\
+	-lpthread\
+	-lSDL2\
 	-lSDL2_image\
 	-lSDL2_mixer\
 	-lxml2 -lz -llzma -licuuc -lm
+
 CFLAGS=\
 	-D_REENTRANT\
 	-DSDL_MAIN_HANDLED\
 	-DWANT_ZLIB\
-	-L/usr/$(TOOLCHAIN)/lib\
-	-I/usr/$(TOOLCHAIN)/include\
 	-I/usr/$(TOOLCHAIN)/include/libxml2\
 	-O2\
 	-pedantic-errors\
@@ -22,8 +28,10 @@ CFLAGS=\
 	-Wall\
 	-Werror\
 	-Wextra
+
 SRCS=\
 	$(wildcard src/*.c)\
 	$(wildcard src/tmx/*.c)\
 	$(wildcard src/inih/*.c)
+
 OBJS=$(patsubst %.c, %.o, $(SRCS))
